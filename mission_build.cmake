12023c62460a597384cff5e856f580f760150033
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-04-18 11:48:44 -0400

    Fix #98, use "GENERATED_FILE" option to generate_config_includefile
    For an EDS build, the include files are generated, and do not yet exist
    when CMake runs.  The generate script now has more error checking than
    it used to and now throws an error if the file does not exist.
    
    Use of the GENERATED_FILE flag causes it to bypass the the search/verify
    and unconditionally link to the file.

--- mission_build.cmake
+++ mission_build.cmake
@@ -46,12 +46,12 @@ endif(CFE_EDS_ENABLED_BUILD)
 foreach(HS_CFGFILE ${HS_MISSION_CONFIG_FILE_LIST})
   get_filename_component(CFGKEY "${HS_CFGFILE}" NAME_WE)
   if (DEFINED HS_CFGFILE_SRC_${CFGKEY})
-    set(DEFAULT_SOURCE "${HS_CFGFILE_SRC_${CFGKEY}}")
+    set(DEFAULT_SOURCE GENERATED_FILE "${HS_CFGFILE_SRC_${CFGKEY}}")
   else()
-    set(DEFAULT_SOURCE "${CMAKE_CURRENT_LIST_DIR}/config/default_${HS_CFGFILE}")
+    set(DEFAULT_SOURCE FALLBACK_FILE "${CMAKE_CURRENT_LIST_DIR}/config/default_${HS_CFGFILE}")
   endif()
   generate_config_includefile(
     FILE_NAME           "${HS_CFGFILE}"
-    FALLBACK_FILE       ${DEFAULT_SOURCE}
+    ${DEFAULT_SOURCE}
   )
 endforeach()
